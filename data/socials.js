011b48274f9b813f29e5fc38bb24875d8e36cef7
Saint-Christ Duga <146033348+GHOST172HD@users.noreply.github.com>
2026-06-16 22:42:08 +0100

    Update socials.js

--- data/socials.js
+++ data/socials.js
@@ -1,5 +1,5 @@
-/* Remplace les URL et identifiants par les contenus publics réels du salon. */
+
 const SOCIAL_ITEMS=[
-  {type:'tiktok',title:'Exemple TikTok',videoId:'6718335390845095173',url:'https://www.tiktok.com/@scout2015/video/6718335390845095173'},
-  {type:'instagram',title:'Publication Instagram à ajouter',url:''}
+  {type:'tiktok',title:'Coiffure',videoId:'7635674125223446017',url:'https://www.tiktok.com/@wizer._/video/7295160311147941125?is_from_webapp=1&sender_device=pc&web_id=7635674125223446017'},
+  {type:'instagram',title:'#NailsNails',url:'https://www.instagram.com/reel/DAa0_IFMvyG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='}
 ];
